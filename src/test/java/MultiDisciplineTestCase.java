5f0acca8a5b9eed7e080d7e0d036e5eb374982e5
lmokaila <lmokaila@inspiredtesting.com>
2022-08-10 07:37:15 +0200

    Completed TP-111,TP-18,TP-4,TP-7

---
+++ src/test/java/MultiDisciplineTestCase.java
@@ -0,0 +1,90 @@
+import Roman.RomanBase;
+import applications.PathCareapplication.PathCareApplication;
+import applications.PathCareapplication.models.AutomationUserModel;
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MultiDisciplineTestCase extends RomanBase {
+
+
+    @Test
+    public void TP_7() throws Exception {
+        Faker faker = new Faker();
+        String[] testcollection = new String[]{"HCD4"};
+        roman()._driver = roman().selenium.getChromeDriver();
+        PathCareApplication pathCare = new PathCareApplication(roman());
+        AutomationUserModel model = AutomationUserModel.getExampleModel("PCLABAssistantGeorge");
+        pathCare.interSystemloginPage.login(model.username, model.password);
+        pathCare.interSystemloginPage.setLocation("PC Depot Admin and Data Capture GEORGE");
+        pathCare.interSystemloginPage.userselection();
+        pathCare.pre_analytical.navigateRegistration();
+        pathCare.pathCareScratch.patientdetails(faker.name().name(), faker.name().lastName(), "11/12/2002","Male");
+        pathCare.pathCareScratch.DoctorSelection();
+        String labespide = pathCare.pathCareScratch.collectiondetailnew("n-1",testcollection);
+        pathCare.interSystemloginPage.changelocation();
+        pathCare.interSystemloginPage.setLocation(model.accessProfile);
+        pathCare.interSystemloginPage.userselection();
+        pathCare.pre_analytical.navigatespecimenRecived();
+        Assert.assertEquals("Specimen already received:",pathCare.pathCareLabSpecimenReception.entryMultipleLabspecimenReception(labespide.concat("-1"),testcollection.length));
+
+    }
+
+    @Test
+    public void TP_4() throws Exception {
+        Faker faker = new Faker();
+        String[] testcollection = new String[]{"HCD4"};
+        roman()._driver = roman().selenium.getChromeDriver();
+        PathCareApplication pathCare = new PathCareApplication(roman());
+        AutomationUserModel model = AutomationUserModel.getExampleModel("PCLABAssistantGeorge");
+        pathCare.interSystemloginPage.login(model.username, model.password);
+        pathCare.interSystemloginPage.setLocation("PC Depot Admin and Data Capture GEORGE");
+        pathCare.interSystemloginPage.userselection();
+        pathCare.pre_analytical.navigateRegistration();
+        pathCare.pathCareScratch.patientdetails(faker.name().name(), faker.name().lastName(), "11/12/2002","Male");
+        pathCare.pathCareScratch.DoctorSelection();
+        String labespide = pathCare.pathCareScratch.collectiondetailnew("n-1",testcollection);
+        Assert.assertNotEquals("",labespide);
+
+    }
+
+    @Test
+    public void TP_18() throws Exception {
+        Faker faker = new Faker();
+        String[] testcollection = new String[]{"HCD4","BUCE"};
+        roman()._driver = roman().selenium.getChromeDriver();
+        PathCareApplication pathCare = new PathCareApplication(roman());
+        AutomationUserModel model = AutomationUserModel.getExampleModel("PCLABAssistantGeorge");
+        pathCare.interSystemloginPage.login(model.username, model.password);
+        pathCare.interSystemloginPage.setLocation("PC Depot Admin and Data Capture GEORGE");
+        pathCare.interSystemloginPage.userselection();
+        pathCare.pre_analytical.navigateRegistration();
+        pathCare.pathCareScratch.patientdetails(faker.name().name(), faker.name().lastName(), "11/12/2002","Male");
+        pathCare.pathCareScratch.DoctorSelection();
+        String labespide = pathCare.pathCareScratch.collectiondetailnew("n-1",testcollection);
+        pathCare.interSystemloginPage.changelocation();
+        pathCare.interSystemloginPage.setLocation(model.accessProfile);
+        pathCare.interSystemloginPage.userselection();
+        pathCare.pre_analytical.navigatespecimenRecived();
+        Assert.assertEquals("Specimen already received:",pathCare.pathCareLabSpecimenReception.entryMultipleLabspecimenReception(labespide.concat("-1"),testcollection.length));
+
+    }
+
+    @Test
+    public void TP_111() throws Exception {
+        Faker faker = new Faker();
+        String[] testcollection = new String[]{"HCD4","BUCE"};
+        roman()._driver = roman().selenium.getChromeDriver();
+        PathCareApplication pathCare = new PathCareApplication(roman());
+        AutomationUserModel model = AutomationUserModel.getExampleModel("PCLABAssistantGeorge");
+        pathCare.interSystemloginPage.login(model.username, model.password);
+        pathCare.interSystemloginPage.setLocation("PC Depot Admin and Data Capture GEORGE");
+        pathCare.interSystemloginPage.userselection();
+        pathCare.pre_analytical.navigateRegistration();
+        pathCare.pathCareScratch.patientdetails(faker.name().name(), faker.name().lastName(), "11/12/2002","Male");
+        pathCare.pathCareScratch.DoctorSelection();
+        String labespide = pathCare.pathCareScratch.collectiondetailnew("n-1",testcollection);
+        Assert.assertNotEquals("",labespide);
+
+    }
+}
